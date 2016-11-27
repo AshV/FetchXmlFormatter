cc9a9b3ae1a923b56acfef648409729573432b1d
Ashish Vishwakarma <Ashish.Vishwakarma@hsd.com.au>
2016-11-27 22:29:29 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,29 @@
+var srcTxt = document.querySelector("#src");
+var csTxt = document.querySelector("#cs");
+var jsTxt = document.querySelector("#js");
+
+srcTxt.onkeyup = function() {
+  var srcValue = new String();
+  srcTxt.value.trim().replace(/"/g, "'").split('\n').forEach(function(line) {
+    if (line.length > 0) {
+      if (line[0] == '+')
+        {}
+      else {
+        if (line[0] == '-') {
+          srcValue += line.substring(1, line.length)+"\n";
+        } else
+          srcValue += line + "\n";
+        format(srcValue);
+      }
+    }
+  });
+};
+
+var format = function(srcValue) {
+  csTxt.value = ('@"' + srcValue.substring(0, srcValue.length - 1) + '"');
+  var jstxt = new String();
+  srcValue.split('\n').forEach(function(line) {
+    jstxt += '"' + line + '"+\n';
+  });
+  jsTxt.value = jstxt.substring(0, jstxt.length - 6);
+};
